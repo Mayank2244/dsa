71965704fbcfc67c45b621652f46749243db9800
Mayank2244 <mayanksehgal9671@gmail.com>
2026-06-04 15:21:03 +0530

    basic node structure

---
+++ LinkedList/basicLL.java
@@ -0,0 +1,32 @@
+
+class Node{
+    int val;
+    Node next;
+
+   //Construction
+    Node(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+class basicLL{
+   public static void main(String[] args) {
+
+
+
+       Node a=new Node(10);
+       Node b=new Node(120);
+       Node c=new Node(130);
+       Node d=new Node(230);
+       a.next=b;
+       b.next=c;
+       c.next=d;
+       d.next=null;
+       System.out.println(a.val);
+       System.out.println(b);
+
+       System.out.println(c);
+       System.out.println(d.val);
+
+   }
+}
