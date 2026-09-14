a1198394bb5f2532209ffb33ad4d3651c607abea
Mayank2244 <mayanksehgal9671@gmail.com>
2026-09-14 21:25:35 +0530

    done linear search by recursion

--- recursion/recursionArrays.java
+++ recursion/recursionArrays.java
@@ -1,4 +1,14 @@
 package recursion;
 
 public class recursionArrays {
+    public static void main(String[] args) {
+        int[] arr={12,10,2,3,34};
+        traverse(arr,0);
+    }
+    public static void traverse(int[]arr,int idx){
+        if(idx==arr.length)return;
+        System.out.print(arr[idx]+" ");
+        traverse(arr,idx+1);
+    }
 }
+
