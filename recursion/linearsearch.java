357c2b24cfb8f9d78db8f835b9a3c9ee7eaadb35
Mayank2244 <mayanksehgal9671@gmail.com>
2026-09-16 21:51:34 +0530

    recursion binary seach and subset string part complete

--- recursion/linearsearch.java
+++ recursion/linearsearch.java
@@ -1,4 +1,18 @@
 package recursion;
 
 public class linearsearch {
+    public static void main(String[] args) {
+        int []arr={1,234,346,6,4,24};
+        int ele=6;
+        System.out.println(find(arr,6,0));
+    }
+    public static boolean find(int[]arr,int ele,int idx){
+        if(idx== arr.length){
+            return false;
+        }
+        if(arr[idx]==ele){
+            return true;
+        }
+        return find(arr,ele,idx+1);
+    }
 }
