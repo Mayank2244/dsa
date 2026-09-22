3beb584ed52f12a94fca575663bd1ed6d03f4eb9
Mayank2244 <mayanksehgal9671@gmail.com>
2026-09-22 14:11:21 +0530

    recursion pre in post,generate parenthiesis,subsetarray done

--- recursion/binarysearch.java
+++ recursion/binarysearch.java
@@ -1,4 +1,25 @@
 package recursion;
+// time complexity log n also the sapce complexity logn
 
 public class binarysearch {
+    public static void main(String[] args) {
+        int []arr={1,23,34,46,647,576,1222};
+//        int target=46;
+        System.out.println(search(arr,46,0,arr.length));
+
+    }
+    public static int search(int []arr,int target,int low,int high){
+        int mid=(low+high)/2;
+        if(low>high)return -1;
+        if(arr[mid]==target) {
+            return mid;
+        }
+        else if(arr[mid]>target){
+            return search(arr,target,mid+1,high);
+
+        }
+        else {
+            return search(arr,target,low,mid-1);
+        }
+    }
 }
--- recursion/subset.java
+++ recursion/subset.java
@@ -1,4 +1,20 @@
 package recursion;
 
 public class subset {
+    public static void main(String[] args) {
+        String s="abc";
+        sebsetcall(" ",s,0);
+    }
+    public static void sebsetcall(String ans,String s,int idx){
+        if(idx==s.length()){
+            System.out.print(ans+" ");
+            return;
+        }
+        char ch=s.charAt(idx);
+        //pick
+        sebsetcall(ans+ch,s,idx+1);
+        //skip
+        sebsetcall(ans,s,idx+1);
+
+    }
 }
