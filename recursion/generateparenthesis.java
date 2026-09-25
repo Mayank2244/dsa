488276544f142242fed6ee80d6d8f5e3cdcd3612
Mayank2244 <mayanksehgal9671@gmail.com>
2026-09-25 21:16:53 +0530

    recursion part check array is sorted and sumofdigits done

--- recursion/generateparenthesis.java
+++ recursion/generateparenthesis.java
@@ -1,4 +1,73 @@
+// normal code
+
+
+
+//package recursion;
+//
+//import java.util.Scanner;
+//
+//public class generateparenthesis {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//
+//   generate(n,0,0," ");
+//
+//    }
+//    public static void generate(int n,int l,int r,String s){
+//        if(r==n&&l==n){
+//            System.out.print(s+" ");
+//            return;
+//        }
+//        if(l<n){
+//            generate(n,l+1,r,s+"(");
+//        }
+//        if(r<l){
+//            generate(n,l,r+1,s+")");
+//        }
+//
+//
+//    }
+//}
+
+
+
+//leetcode version
 package recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class generateparenthesis {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of n:");
+        int n=sc.nextInt();
+
+        List<String>ans=new ArrayList<>();
+        System.out.println(generateParenthesis(n));
+
+    }
+    public static List<String>generateParenthesis(int n){
+        List<String>ans=new ArrayList<>();
+        generate(n,0,0," ",ans);
+        return ans;
+    }
+
+    public static void generate(int n,int l,int r,String s,List<String>ans){
+        if(l==n&&r==n){
+            ans.add(s);
+            return;
+        }
+        if(l<n){
+            generate(n,l+1,r,s+"(",ans);
+        }
+        if(r<l){
+            generate(n,l,r+1,s+")",ans);
+        }
+
+
+    }
 }
+
--- recursion/subsetarray.java
+++ recursion/subsetarray.java
@@ -1,4 +1,20 @@
 package recursion;
 
+import java.util.List;
+
 public class subsetarray {
+    static void main(String[] args) {
+        int []arr={1,2,3};
+        subset(arr,0,"");
+    }
+    public static void subset(int[]arr, int idx, String ans){
+        if(idx==arr.length){
+            System.out.print(ans+" ");
+            return;
+        }
+        subset(arr,idx+1,ans+arr[idx]);
+        //dont take
+        subset(arr,idx+1,ans);
+
+    }
 }
